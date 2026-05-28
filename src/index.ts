a869725566a7ad2dfe2966e11e95991d35db640b
Martin Gouthier <martin.gouthier1@etu.univ-lorraine.fr>
2026-05-28 17:25:22 +0200

    Question 1.1.c

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+import {loadPicture} from "./photoloader";
+
+async function getPicture(id:number):Promise<void>{
+    const photo = await loadPicture(id);
+    if (photo != null){
+        console.log(`Titre : ${photo.titre}, Type de fichier : ${photo.type}, URL : ${photo.url.href}`);
+    } else {
+        console.log(`Aucune photo trouvée pour ${id}`);
+    }
+}
+
+const nbr = window.location.hash.startsWith('#')
+    ? window.location.hash.substring(1)
+    : window.location.hash;
+
+getPicture(parseInt(nbr))
