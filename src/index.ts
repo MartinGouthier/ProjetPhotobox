91452ed73db2928502ae1afebc9ea66effa8c752
YanisH54 <yanis.husser2@etu.univ-lorraine.fr>
2026-05-29 02:46:02 +0200

    ajout de la gestion des erreurs dans index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import {loadPicture} from "./photoloader";
+import { loadPicture } from "./photoloader";
 
-async function getPicture(id:number):Promise<void>{
-    const photo = await loadPicture(id);
-    if (photo != null){
+async function getPicture(id: number): Promise<void> {
+    try {
+        const photo = await loadPicture(id);
         console.log(`Titre : ${photo.titre}, Type de fichier : ${photo.type}, URL : ${photo.url.href}`);
-    } else {
+    } catch (erreur) {
         console.log(`Aucune photo trouvée pour ${id}`);
     }
 }
 
-const nbr = window.location.hash.startsWith('#')
+// On récupère le numéro de l'image dans l'URL, sinon on charge la numéro 6 par défaut.
+const hashText = window.location.hash.startsWith('#')
     ? window.location.hash.substring(1)
     : window.location.hash;
+ 
+const nbr = parseInt(hashText);
 
-getPicture(parseInt(nbr))
+getPicture(isNaN(nbr) ? 6 : nbr);
