e221a3dd19914e8852556e8c067cd26a172fa9a9
YanisH54 <yanis.husser2@etu.univ-lorraine.fr>
2026-05-29 02:27:49 +0200

    ajout de la gestion des erreurs dans photoloader

--- src/photoloader.ts
+++ src/photoloader.ts
@@ -1,19 +1,25 @@
-import "./types";
-import {Photo, ReponsePhoto} from "./types";
-import {API_URL} from "./config";
+import { Photo, ReponsePhoto } from "./types";
+import { API_URL } from "./config";
 
-
-export const loadPicture = async function (idPicture: number): Promise<Photo | null> {
-
-    const requete = await fetch(API_URL+`photos/${idPicture}`);
-    if (requete.ok) {
-        return requete.json()
-            .then((reponse: ReponsePhoto) => {
-                return reponse.photo;
-            });
-    } else
-        return null;
+export function loadPicture(idPicture: number): Promise<Photo> {
+    
+    return fetch(`${API_URL}photos/${idPicture}`, { credentials: 'include' })
+        .then((reponse: Response): Promise<Photo> => {
+            
+            // Le serveur a répondu mais il signale une erreur HTTP, on force donc l'echec de la promesse
+            if (!reponse.ok) {
+                return Promise.reject(new Error(`Erreur HTTP : ${reponse.statusText}`));
+            }
+            
+            return reponse.json().then((data: ReponsePhoto) => data.photo);
+        })
+        .catch((erreur: unknown) => {
+            
+            if (erreur instanceof Error) {
+                console.error("Problème rencontré :", erreur.message);
+            }
+            
+            // On relance l'erreur pour prévenir la fonction appelante
+            return Promise.reject(erreur);
+        });
 }
-
-
-
