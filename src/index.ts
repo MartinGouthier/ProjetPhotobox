6d541b55879bbb4629c895c5f7643d65951c4d56
YanisH54 <yanis.husser2@etu.univ-lorraine.fr>
2026-05-29 03:29:01 +0200

    ajout de la 2b

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { loadPicture } from "./photoloader";
+import { displayPicture } from "./ui"; 
 
 async function getPicture(id: number): Promise<void> {
     try {
-        const photo = await loadPicture(id);
-        console.log(`Titre : ${photo.titre}, Type de fichier : ${photo.type}, URL : ${photo.url.href}`);
+        const reponse = await loadPicture(id);
+        displayPicture(reponse);
     } catch (erreur) {
-        console.log(`Aucune photo trouvée pour ${id}`);
+        console.error(`Aucune photo trouvée pour ${id}`);
     }
 }
 
-// On récupère le numéro de l'image dans l'URL, sinon on charge la numéro 6 par défaut.
 const hashText = window.location.hash.startsWith('#')
     ? window.location.hash.substring(1)
     : window.location.hash;
