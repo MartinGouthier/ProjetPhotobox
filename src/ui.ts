36e016aa7502eba0252ae5ef16554b83f88e35c7
YanisH54 <yanis.husser2@etu.univ-lorraine.fr>
2026-05-29 03:07:39 +0200

    ajout de l'étape 2 de l'exercice 1

---
+++ src/ui.ts
@@ -0,0 +1,29 @@
+import Handlebars from 'handlebars';
+import { Photo } from './types';
+
+const sourceTemplate = `
+<article>
+    <h2>{{titre}}</h2>
+
+    <img src="{{url.href}}" alt="Photo de {{titre}}">
+
+    <ul>
+        <li>Type : {{type}}</li>
+        <li>Format : {{format}}</li>
+        <li>Dimensions : {{width}} x {{height}} px</li>
+        <li>Poids : {{size}} octets</li>
+    </ul>
+
+    <p>{{descr}}</p>
+</article>
+`;
+
+const template = Handlebars.compile(sourceTemplate);
+
+export function displayPicture(photo: Photo): void {
+    const conteneur = document.querySelector('#la_photo');
+
+    // On injecte l'objet photo dans le template 
+    conteneur!.innerHTML = template(photo);
+
+}
