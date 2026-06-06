1cdaa7d42872b958403ab05c7262486aed7d5d43
Martin Gouthier <martin.gouthier1@etu.univ-lorraine.fr>
2026-06-06 21:13:26 +0200

    Factorisation loader ressource

--- src/photoloader.ts
+++ src/photoloader.ts
@@ -2,29 +2,10 @@ import { Photo, ReponsePhoto } from "./types";
 import { BASE_URL, API_URL } from "./config";
 
 export function loadPicture(idPicture: number): Promise<ReponsePhoto> {
-    
-    return fetch(`${API_URL}photos/${idPicture}`, { credentials: 'include' })
-        .then((reponse: Response): Promise<ReponsePhoto> => {
-            
-            // Le serveur a répondu mais il signale une erreur HTTP, on force donc l'echec de la promesse
-            if (!reponse.ok) {
-                return Promise.reject(new Error(`Erreur HTTP : ${reponse.statusText}`));
-            }
-            
-            return reponse.json();
-        })
-        .catch((erreur: unknown) => {
-            
-            if (erreur instanceof Error) {
-                console.error("Problème rencontré :", erreur.message);
-            }
-            
-            // On relance l'erreur pour prévenir la fonction appelante
-            return Promise.reject(erreur);
-        });
+    return loadResource(`photos/${idPicture}`);
 }
 export function loadResource(uri: string): Promise<any> {
-    const urlComplete = `${BASE_URL}${uri}`;
+    const urlComplete = `${API_URL}${uri}`;
     
     return fetch(urlComplete, { credentials: 'include' })
         .then((reponse: Response) => {
@@ -32,5 +13,13 @@ export function loadResource(uri: string): Promise<any> {
                 return Promise.reject(new Error(`Erreur HTTP : ${reponse.statusText}`));
             }
             return reponse.json();
+        }).catch((erreur: unknown) => {
+
+            if (erreur instanceof Error) {
+                console.error("Problème rencontré :", erreur.message);
+            }
+
+            // On relance l'erreur pour prévenir la fonction appelante
+            return Promise.reject(erreur);
         });
 }
